1e9d56ea9803472dfb4c61856d3522c8c592982c
Alexander Feng <alexanderfzz.haha@gmail.com>
2023-03-03 08:53:31 -0500

    test if config is correct

--- Player.java
+++ Player.java
@@ -1,2 +1,23 @@
 public class Player {
+    String name;
+    int capital;
+    double VIPI;
+
+    public Player(String name, int capital) {
+        this.name = name;
+        this. capital = capital;
+    }
+
+    public void withdrawCAptial(int amount) {
+        this.capital -= amount;
+    }
+    public void depositCapital(int amount) {
+        this.capital += amount;
+    }
+    public void leaveTable() {
+        //TODO: pop player from existing table
+    }
+    public void showHand() {
+        // if endgamecondition perform action
+    }
 }
